6424b31fec7cb6f30b2d7a96291a4d43cae311b4
bhaskarajmera <bhaskarajmera246@gmail.com>
2026-03-13 21:11:07 +0530

    authenticated session only, on /dashboard/[id]

--- src/app/dashboard/[id]/page.js
+++ src/app/dashboard/[id]/page.js
@@ -1,10 +1,22 @@
 "use client"
 
-import { useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import TimesheetDetail from "@/components/TimesheetDetail"
+import { useSession } from "next-auth/react"
+import { useEffect } from "react"
 
 export default function TimesheetDetailPage() {
     const { id } = useParams() // get timesheet id from URL
+    const router = useRouter()
+    const { data: session, status } = useSession()
+
+    useEffect(() => {
+        if (status === "loading") return
+
+        if (!session) {
+            router.push("/login")
+        }
+    }, [session, status, router])
 
     return (
         <div>
